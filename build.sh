4a5781fcdc03540321ca626ec83224834b71f951
Gnfw <ggnsyp@gmail.com>
2025-07-09 19:38:53 +0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+set -e
+
+# Установка зависимостей с предпочтением бинарных дистрибутивов
+pip install --upgrade pip
+pip install --prefer-binary -r requirements.txt
+
+# Установка данных для TextBlob
+python -m textblob.download_corpora
