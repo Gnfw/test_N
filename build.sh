350b5917a5380bf351ddbec3e64b6a20ad9cefc4
Gnfw <ggnsyp@gmail.com>
2025-07-09 20:34:35 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Установка шрифтов
+echo "--- Installing DejaVu fonts ---"
+mkdir -p fonts
+if [ ! -f "fonts/DejaVuSans.ttf" ]; then
+    wget -q -O fonts/dejavu.zip https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_2_37/dejavu-fonts-ttf-2.37.zip
+    unzip -q fonts/dejavu.zip -d fonts_temp
+    cp fonts_temp/dejavu-fonts-ttf-2.37/ttf/DejaVuSans.ttf fonts/
+    rm -rf fonts_temp dejavu.zip
+fi
+
 echo "=== Installing dependencies ==="
 python -m pip install --upgrade pip
 pip install -r requirements.txt
